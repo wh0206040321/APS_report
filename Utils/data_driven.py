4b4b11b330ab4176dc972d036429a79e4c88d244
wh0206040321 <1121470915@qq.com>
2025-08-29 17:50:49 +0800

    更新代码

--- Utils/data_driven.py
+++ Utils/data_driven.py
@@ -4,10 +4,3 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 
 
-class DateDriver:
-    url = "http://wkawka.vicp.net:27890"
-    driver_path = 'D:/Program Files/Python310/chromedriver.exe'
-    username = 'hongaoqing'
-    password = 'Qw123456'  # 1234qweR
-    planning = '金属（演示）'
-    URL_RETRY_WAIT = int(os.getenv("URL_RETRY_WAIT", 60))
